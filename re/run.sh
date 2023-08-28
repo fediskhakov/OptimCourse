a1a914bf00975157645864ba2b56e8d16e56a32b
Fedor Iskhakov <fediskhakov@gmail.com>
2023-08-28 20:49:31 +0400

    Lecture 6 first draft, more work is needed

--- re/run.sh
+++ re/run.sh
@@ -2,19 +2,17 @@
 
 # Script to convert question bank from yaml to xml
 PATTERNS=conversions.yml
-FILE=../05.linear_algebra.md_orig
-OUTPUT=../05.linear_algebra_converted.md
-FILE=../05.linear_algebra.md_orig
-OUTPUT=../05.linear_algebra_converted.md
+FILE=../06.optimization_fundamentals.md0
+OUTPUT=../06.optimization_fundamentals.md
 
 # python3 repl.py $PATTERNS $FILE -o $OUTPUT
 
-FILE=../05.exercises.D.md
-OUTPUT=../05.exercises.D.md
+FILE=../06.exercises.E.md
+OUTPUT=../06.exercises.E.md
 
-python3 repl.py $PATTERNS $FILE -o $OUTPUT
+# python3 repl.py $PATTERNS $FILE -o $OUTPUT
 
-FILE=../../01.solutions/exercise_solutions/05.solutions_D.md 
-OUTPUT=../../01.solutions/exercise_solutions/05.solutions_D.md 
+FILE=../../01.solutions/exercise_solutions/06.solutions_E.md 
+OUTPUT=../../01.solutions/exercise_solutions/06.solutions_E.md 
 
 python3 repl.py $PATTERNS $FILE -o $OUTPUT
