32250e584c2a25a82f2ea5221583c343c41b5c0d
Fedor Iskhakov <fediskhakov@gmail.com>
2023-09-28 09:39:33 +1000

    Lecture 8 + buid

--- re/run.sh
+++ re/run.sh
@@ -2,17 +2,13 @@
 
 # Script to convert question bank from yaml to xml
 PATTERNS=conversions.yml
-FILE=../06.optimization_fundamentals.md0
-OUTPUT=../06.optimization_fundamentals.md
+FILE=../08.temp.md
+OUTPUT=../08.temp_out.md
 
 # python3 repl.py $PATTERNS $FILE -o $OUTPUT
-
-FILE=../06.exercises.E.md
-OUTPUT=../06.exercises.E.md
-
 # python3 repl.py $PATTERNS $FILE -o $OUTPUT
 
-FILE=../../01.solutions/exercise_solutions/06.solutions_E.md 
-OUTPUT=../../01.solutions/exercise_solutions/06.solutions_E.md 
+# FILE=../../01.solutions/exercise_solutions/06.solutions_E.md 
+# OUTPUT=../../01.solutions/exercise_solutions/06.solutions_E.md 
 
 python3 repl.py $PATTERNS $FILE -o $OUTPUT
--- temp.py
+++ temp.py
@@ -10,29 +10,27 @@ def f(x,y):
     return -x**2 - 2* y**2 
 
 if __name__ == '__main__':
-    A = np.array([[1,.5],[.5,1.5]])
-    # A = np.array([[2,0],[0,1]])
-    f = lambda x: -(x@A@x)
-    x = y = np.linspace(-5.0, 5.0, 100)
+    f = lambda x: x[0]**3/3 - 3*x[1]**2 + 5*x[0] - 6*x[0]*x[1]
+
+    x = y = np.linspace(-10.0, 10.0, 100)
     X, Y = np.meshgrid(x, y)
     zs = np.array([f((x,y)) for x,y in zip(np.ravel(X), np.ravel(Y))])
     Z = zs.reshape(X.shape)
 
-    fig = plt.figure(dpi=160)
-    ax1 = fig.add_subplot(111, projection='3d')
-    ax1.plot_surface(X, Y, Z, 
-                rstride=2, 
-                cstride=2,
-                cmap=cm.jet,
-                alpha=0.7,
-                linewidth=0.25)
-    plt.setp(ax1,xticks=[],yticks=[],zticks=[])
+    a,b=4,8
+    # (x/a)^2 + (y/b)^2 = 1
+    theta = np.linspace(0, 2 * np.pi, 100)
+    X1 = a*np.cos(theta)
+    Y1 = b*np.sin(theta)
+    zs = np.array([f((x,y)) for x,y in zip(np.ravel(X1), np.ravel(Y1))])
+    Z1 = zs.reshape(X1.shape)
 
     fig = plt.figure(dpi=160)
     ax2 = fig.add_subplot(111)
     ax2.set_aspect('equal', 'box')
     ax2.contour(X, Y, Z, 50,
                 cmap=cm.jet)
+    ax2.plot(X1, Y1)
     plt.setp(ax2, xticks=[],yticks=[])
 
     fig = plt.figure(dpi=160)
@@ -43,10 +41,9 @@ if __name__ == '__main__':
                 alpha=0.7,
                 linewidth=0.25)
     f0 = f(np.zeros((2)))+0.1
-    ax3.scatter(0, 0, f0, c='black', marker='o', s=10)
-    ax3.plot([-3,3],[0,0],[f0,f0],color='black')
-    ax3.plot([0,0],[-3,3],[f0,f0],color='black')
+    ax3.plot(X1, Y1, Z1, c='red')
     plt.setp(ax3,xticks=[],yticks=[],zticks=[])
+    ax3.view_init(elev=18, azim=154)
 
     plt.show()
 
