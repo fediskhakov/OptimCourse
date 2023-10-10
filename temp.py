87600324d99da64ba588678308e5e0515c32ec48
Fedor Iskhakov <fediskhakov@gmail.com>
2023-10-11 01:31:38 +1100

    Solutions for exercise set H

--- temp.py
+++ temp.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-def f(x,y):
-    return -x**2 - 2* y**2 
-
 if __name__ == '__main__':
     # f = lambda x: x[0]**3/3 - 3*x[1]**2 + 5*x[0] - 6*x[0]*x[1]
 
